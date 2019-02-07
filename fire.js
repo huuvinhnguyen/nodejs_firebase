4217f1990bc577a99db6470d0a7355c2b17692b8
Vinh Nguyen <huuvinh@live.com>
2019-02-07 23:12:04 +0700

    Update fire.js

--- fire.js
+++ fire.js
@@ -1,12 +1,12 @@
 var firebase = require('firebase')
 // change lines below with your own Firebase snippets!
 var config = {
-  apiKey: "AIzaSyDSLRypcYx2yHSqpVXJMBrzYw1xAHZtgYI",
-  authDomain: "test-iot-esp.firebaseapp.com",
-  databaseURL: "https://test-iot-esp.firebaseio.com",
-  projectId: "test-iot-esp",
-  storageBucket: "test-iot-esp.appspot.com",
-  messagingSenderId: "467772798973"
+  apiKey: "AIzaSyA1GUbp9tPz7Butdhb2u693pn1UzbIb5Jo",
+  authDomain: "nodejs-b61db.firebaseapp.com",
+  databaseURL: "https://nodejs-b61db.firebaseio.com",
+  projectId: "nodejs-b61db",
+  storageBucket: "nodejs-b61db.appspot.com",
+  messagingSenderId: "334945148087"
 };
 const fire = firebase.initializeApp(config);
 module.exports = fire;
